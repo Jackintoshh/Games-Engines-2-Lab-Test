abbf108d660db8c49dd13dd79a5078c64cb051db
Jack Wright <jjwright10@hotmail.com>
2020-04-21 11:01:43 +0100

    Lights spawn in circle and change colour

---
+++ Assets/Scripts/LightProperties.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightProperties : MonoBehaviour
+{
+    bool isActive = false;
+    bool isChanging = false;
+    bool isNotActive = false;
+    Color lightCol;
+    float time;
+    int randTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = (int)Random.Range(0, 3);
+        //Debug.Log(rand);
+        if(rand == 0)
+        {
+            lightCol = Color.green;
+            this.GetComponent<Renderer>().material.color = lightCol;
+            isActive = true;
+        }
+        if (rand == 1)
+        {
+            lightCol = Color.yellow;
+            this.GetComponent<Renderer>().material.color = lightCol;
+            isChanging = true;
+        }
+        if (rand == 2)
+        {
+            lightCol = Color.red;
+            this.GetComponent<Renderer>().material.color = lightCol;
+            isNotActive = true;
+        }
+
+        randTime = (int)Random.Range(5, 11);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        Debug.Log(randTime);
+
+        if(time > randTime && isActive)
+        {
+            time = 0f;
+            isChanging = true;
+            isActive = false;
+            lightCol = Color.yellow;
+            this.GetComponent<Renderer>().material.color = lightCol;
+
+            //randTime = (int)Random.Range(5, 11);
+
+            if (isChanging && time > 4f)
+            {
+                time = 0f;
+                lightCol = Color.red;
+                isNotActive = true;
+                isChanging = false;
+
+                this.GetComponent<Renderer>().material.color = lightCol;
+                randTime = (int)Random.Range(5, 11);
+            }
+        }
+
+        if (time > randTime && isNotActive)
+        {
+            time = 0f;
+            isActive = true;
+            lightCol = Color.green;
+            this.GetComponent<Renderer>().material.color = lightCol;
+            
+            randTime = (int)Random.Range(5, 11);
+        }
+
+        if (isChanging && time > 4f)
+        {
+            time = 0f;
+            lightCol = Color.red;
+            isNotActive = true;
+            isChanging = false;
+
+            this.GetComponent<Renderer>().material.color = lightCol;
+            randTime = (int)Random.Range(5, 11);
+        }
+
+    }
+}
---
+++ Assets/Scripts/LightsSpawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsSpawn : MonoBehaviour
+{
+
+    List<GameObject> lights = new List<GameObject>();
+    public GameObject lightPref;
+    LightsColourChange lgc;
+    Vector3 lightLoc = new Vector3();
+    public int numLights = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lights = 
+
+        for (int i = 0; i < numLights; i++)
+        {
+            GameObject Light = GameObject.Instantiate(lightPref, this.transform);
+            lights.Add(Light);
+            Light.transform.SetParent(this.transform);
+
+            int rand = Random.Range(0, 100);
+            Light.transform.position = this.gameObject.transform.position;
+            Light.transform.eulerAngles = new Vector3(0, 36 * i, 0);
+            Light.transform.Translate(Vector3.forward * 10);
+        }
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
